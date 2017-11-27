29c3071e4a81e94c0cbef7942978172ed2aebbb0
unknown <amoutonbrady@gmail.com>
2017-11-27 03:25:02 +0100

    Cleaned up

--- .fusebox/cache/2.5.0-beta.1/F6gbH-bundle/static/components%2Findex.js
+++
@@ -1,8 +0,0 @@
-module.exports = { contents: "'use strict';\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\nexports.Card = undefined;\n\nvar _Card = require('./Card');\n\nvar _Card2 = _interopRequireDefault(_Card);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\nvar Card = exports.Card = _Card2.default;",
-dependencies: ["./Card"],
-sourceMap: "{\"version\":3,\"sources\":[\"/components/index.js\"],\"names\":[\"Card\"],\"mappings\":\";;;;;;;AAAA;;;;;;AAEO,IAAMA,oCAAN\",\"file\":\"components/index.js\",\"sourcesContent\":[\"import C from './Card';\\r\\n\\r\\nexport const Card = C;\\r\\n\"]}",
-headerContent: undefined,
-mtime: 1511745306616,
-devLibsRequired : undefined,
-_ : {}
-}
